36a5d09cae5de5fa358928f0df0ea68f8a8d9c4c
laryn <laryn@cedc.org>
2021-01-11 10:59:05 -0600

    WIP dropbutton separate edit

---
+++ js/paragraphs_separate_edit.js
@@ -0,0 +1,5 @@
+(function($, Backdrop) {
+    Backdrop.ajax.prototype.commands.paragraphs_separate_edit_message_hide = function(ajax, response, status) {
+        $('.paragraphs-separate-edit-message').delay(1000).fadeOut();
+    }
+})(jQuery, Backdrop);
