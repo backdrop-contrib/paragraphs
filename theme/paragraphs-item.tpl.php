6428ffa1fb24f662efc7a794602582fa0dd21f16
Alejandro Cremaschi <argiepiano@yahoo.com>
2021-12-05 06:19:13 -0700

    Issue #108: Use $classes_array instead of $classes to apply css classes
    Fixes #108

--- theme/paragraphs-item.tpl.php
+++ theme/paragraphs-item.tpl.php
@@ -10,24 +10,19 @@
  *   all, or print a subset such as render($content['field_example']). Use
  *   hide($content['field_example']) to temporarily suppress the printing of a
  *   given element.
- * - $classes: String of classes that can be used to style contextually through
- *   CSS. It can be manipulated through the variable $classes_array from
- *   preprocess functions. By default the following classes are available, where
+ * - $classes_array: An array of classes that can be used to style contextually through
+ *   CSS. By default the following classes are available, where
  *   the parts enclosed by {} are replaced by the appropriate values:
- *   - entity
  *   - entity-paragraphs-item
  *   - paragraphs-item-{bundle}
  *
- * Other variables:
- * - $classes_array: Array of html class attribute values. It is flattened into
- *   a string within the variable $classes.
  *
  * @see template_preprocess()
  * @see template_preprocess_entity()
  * @see template_process()
  */
 ?>
-<div class="<?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
+<div class="<?php print implode(' ', $classes_array); ?>"<?php print backdrop_attributes($attributes); ?>>
   <?php print render($admin_links); ?>
   <?php print render($title_prefix); ?>
   <?php print render($title_suffix); ?>
