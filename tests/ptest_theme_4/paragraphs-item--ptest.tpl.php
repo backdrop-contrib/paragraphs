b8682ed5520ac47b14052ea725af1e99677a888c
Mark Burgess <mark@zymosis.com>
2026-03-02 12:59:34 -0800

    Issue 248: Test that correct templates are called.

---
+++ tests/ptest_theme_4/paragraphs-item--ptest.tpl.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation for a single paragraph item.
+ *
+ * Available variables:
+ * - $admin_links: A dropbutton render array, if separate editing is turned on.
+ * - $content: An array of content items. Use render($content) to print them
+ *   all, or print a subset such as render($content['field_example']). Use
+ *   hide($content['field_example']) to temporarily suppress the printing of a
+ *   given element.
+ * - $classes_array: An array of classes that can be used to style contextually through
+ *   CSS. By default the following classes are available, where
+ *   the parts enclosed by {} are replaced by the appropriate values:
+ *   - entity-paragraphs-item
+ *   - paragraphs-item-{bundle}
+ *
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_entity()
+ * @see template_process()
+ */
+?>
+<div id="<?php print $variables['html_id']; ?>" class="<?php print implode(' ', $classes_array); ?>"<?php print backdrop_attributes($attributes); ?>>
+  <?php print render($admin_links); ?>
+  <?php print render($title_prefix); ?>
+  <?php print render($title_suffix); ?>
+  <div class="content"<?php print backdrop_attributes($content_attributes); ?>>
+    <?php print render($content); ?>
+  </div>
+</div>
+<p>This is the bundle template.</p>
+
